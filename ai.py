0fbe7e6cad3636f125fc5f2d4ddb60f254ee342f
Mohamed Eid <m.eid@seqoon.com>
2025-09-30 19:01:43 +0300

    implemented ai and retriever modules

--- ai.py
+++ ai.py
@@ -1,4 +1,6 @@
 from typing import Dict, Any
+from retriever import retriever
+
 
 PROMPT = """
 Find recent tweets about {topic} from {location} written in {language}.
@@ -13,12 +15,30 @@ class FakeAI:
     # the query parameter is the string after injecting the keys from the context into the PROMPT
     async def __retrieve(self, query: str) -> Dict[str, Any]:
         # Write code here
+        retrieved_docs = retriever.similarity_search(query, k=1)
+        if retrieved_docs:
+            doc = retrieved_docs[0]
+            return {"tweet": doc.page_content, "author": doc.metadata}
+        else:
+            # we may raise an exception here if no documents found. like:
+            # raise Exception(f"No documents found in vector store for query: {query}")
+            # and handle it in the generate_response function
+            # but for now, we'll just return a fake tweet and author as a fallback
+            pass
         return {"tweet": "fake tweet", "author": {"name": "John Doe", "email": "john@example.com"}}
 
     # TODO: Inject the keys from the context into the PROMPT
     async def generate_response(self, context: Dict[str, Any]) -> Dict[str, Any]: 
         # Write code here
-        return await self.__retrieve("prompt with injected keys")
+        # I provided default values for topic, location, and language if they are not in the context
+        injected_prompt = PROMPT.format(
+            topic=context.get("topic", "general"),
+            location=context.get("location", "world"),
+            language=context.get("language", "English")
+        )
+
+
+        return await self.__retrieve(injected_prompt)
     
     
 
--- retriever.py
+++ retriever.py
@@ -13,9 +13,28 @@ embeddings = FakeEmbeddings(size=768)
 # TODO: Load data/tweets.json
 # page content is the tweet text
 # the metadata is the author information
+"""
+tweet sample:
+  {
+    "author": {
+      "name": "Elena Petrova",
+      "email": "elena.petrova@example.com"
+    },
+    "tweet": "Just deployed our microservice architecture using #FastAPI and Docker "
+  }
+  default fake tweet:
+  {"tweet": "fake tweet", "author": {"name": "John Doe", "email": "john@example.com"}}
+"""
 tweets = []
-
-
-
+with open("data/tweets.json", "r") as f:
+    tweets_list = json.load(f)
+    for tweet in tweets_list:
+        # I used the dict.get method to provide default values as fallback in case the keys are missing (used fake tweet)
+        tweets.append(
+            Document(
+                page_content=tweet.get("tweet", "fake tweet"),
+                metadata=tweet.get("author", {"name": "John Doe", "email": "john@example.com"})
+                )
+        )
 
 retriever = FAISS.from_documents(tweets, embeddings)
